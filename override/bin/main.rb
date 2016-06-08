b33d4de5c550dc802cc4632ad986183a1778d421
Neha Tandon <nehatandon1@gmail.com>
2016-06-08 11:27:59 +0530

    Add Override to_s

---
+++ override/bin/main.rb
@@ -0,0 +1,5 @@
+require_relative "../lib/string.rb"
+
+puts "Enter desired string\n"
+str = gets.chomp
+puts str.to_s
---
+++ override/lib/string.rb
@@ -0,0 +1,9 @@
+# Overwrite the default 'to_s' method such that it inverses the case of each letter.
+# Eg: "hello WORLD".to_s ­> "HELLO world"
+
+class String
+  
+  def to_s
+    self.swapcase
+  end
+end
