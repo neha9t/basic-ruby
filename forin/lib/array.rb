93274449ae390d285e9dab0c946ec1f786c75c3c
Neha <neha@pstakecare.com>
2016-06-21 09:54:55 +0530

    Add folders

---
+++ forin/lib/array.rb
@@ -0,0 +1,4 @@
+ # Create a method for Array that can create a hash having 'key' as length of the element 
+ # and value as an array of all the elements of that length. Make use of for­in loop for the purpose.
+# Eg: ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ'] should give result as 
+# {3=>["abc", "def", 234], 4=>[1234, "abcd", "mnop", "zZzZ"], 1=>["x", 5]}
---
+++ inject/lib/harsh.rb
@@ -0,0 +1,3 @@
+# Write a method that groups the above hash into 2 groups of 'even' and 'odd' length using 'inject'.
+# Eg: ['abc','def',1234,234,'abcd','x','mnop',5,'zZzZ'] ­> {odd: [["x", 5], 
+# ["abc", "def", 234]], even: [[1234, "abcd", "mnop", "zZzZ"]]}
